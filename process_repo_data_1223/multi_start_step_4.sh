172f4dcb10a28f41af4f75f8316c02bc359e56e5
feishan <feishan@minimaxi.com>
2025-12-31 18:58:29 -0500

    add

--- process_repo_data_1223/multi_start_step_4.sh
+++ process_repo_data_1223/multi_start_step_4.sh
@@ -1,11 +1,11 @@
 
 mkdir -p step_4_1231_logs
 
-for i in $(seq 1 50); do
+for i in $(seq 1 100); do
   nohup python step_4_fim_completion_and_critique_1230.py \
     -i /data/yubo/datasets/process_data_output_1228/step_3_res_data_1231/step_3_results_merged_1231.json \
     -o /data/yubo/datasets/process_data_output_1228/step_3_res_data_1231/step_4_results_merged_1231.json \
-    --shard $i --total-shards 50 \
+    --shard $i --total-shards 100 \
     --wandb --wandb-run-name "exp_step_4_1231" \
     > step_4_1231_logs/shard_$i.log 2>&1 &
 done
