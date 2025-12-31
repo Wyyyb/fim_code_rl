258a1e7540da047340942c616f7a9bee123ead93
feishan <feishan@minimaxi.com>
2025-12-30 20:05:03 -0500

    add

--- process_repo_data_1223/multi_start_step_4.sh
+++ process_repo_data_1223/multi_start_step_4.sh
@@ -2,7 +2,7 @@
 mkdir -p step_4_test_logs
 
 for i in $(seq 1 5); do
-  nohup python step_4_fim_completion_and_critique.py \
+  nohup python step_4_fim_completion_and_critique_1230.py \
     -i /data/yubo/datasets/process_data_output_1228/step_3_bk_1229/step_3_results_merged_1230.json \
     -o /data/yubo/datasets/process_data_output_1228/step_3_bk_1229/step_4_results_merged_1230_test.json \
     --shard $i --total-shards 5 \
