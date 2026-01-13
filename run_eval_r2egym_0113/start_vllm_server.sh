637e069b5507111dbb8ccaf8fbf2673abff47bf9
feishan <feishan@minimaxi.com>
2026-01-13 17:37:50 -0500

    add

--- run_eval_r2egym_0113/start_vllm_server.sh
+++ run_eval_r2egym_0113/start_vllm_server.sh
@@ -4,6 +4,8 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 export MAX_CONTEXT_LEN=65536
 VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 vllm serve /data/yubowang/fim_sft_ckpts/ori_qwen25_coder_7b_ins_r2egym_sft_0108-ckpt_808 \
+    --host 0.0.0.0 \
+    --port 9002 \
     --tensor-parallel-size 4 \
     --max-model-len $MAX_CONTEXT_LEN \
     --hf-overrides '{"max_position_embeddings": '$MAX_CONTEXT_LEN'}' \
