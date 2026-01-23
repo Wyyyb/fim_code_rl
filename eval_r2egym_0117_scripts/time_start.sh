8e721f177b76b330c587458dba6cc7d879c04b7e
feishan <feishan@minimaxi.com>
2026-01-23 04:16:37 -0500

    add

---
+++ eval_r2egym_0117_scripts/time_start.sh
@@ -0,0 +1,9 @@
+
+cd /data2/yubo/sft_ckpts/ori_qwen25_coder_14b_ins_r2egym_0122/v1-20260123-054036/checkpoint-806/
+
+huggingface-cli upload ubowang/ori_qwen25_coder_14b_ins_r2egym_0122 . --repo-type model
+
+cd /data2/yubo/fim_code_rl/eval_r2egym_0117_scripts
+
+bash sft_r2egym_fim_ckpt_1659_qwen_14b_0122.sh
+
