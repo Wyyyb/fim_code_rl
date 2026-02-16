45e32fb9b09ef39b8eee84574a9ceb46ff056a0a
feishan <feishan@minimaxi.com>
2026-02-16 11:13:12 +0800

    add

--- curate_fim_dataset_0215/step_3_dep_selection_0216.py
+++ curate_fim_dataset_0215/step_3_dep_selection_0216.py
@@ -614,7 +614,7 @@ def process_samples(
     skipped_too_short = 0
     total_selected_functions = 0
 
-    for idx, sample in tqdm(enumerate(samples[-1000:])):
+    for idx, sample in tqdm(enumerate(samples[-10000:])):
         out = dict(sample)  # shallow copy
         code = out.get("code_content", "")
         sid = out.get("sample_id", idx)
