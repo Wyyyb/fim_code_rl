51ad8e53e575733107d8c3ed0c29690989ac978e
feishan <feishan@minimaxi.com>
2026-02-18 10:00:13 +0800

    add

---
+++ curate_fim_dataset_0215/multi_start_step_4_try.sh
@@ -0,0 +1,12 @@
+
+
+for i in $(seq 1 50); do
+    nohup python try_step_4_gemini_fim_0218.py \
+      -i /data/yubo/datasets/process_data_output_0215/step_3_selected_fim_functions_0215_functions_sorted_0218.json \
+      -o /data/yubo/datasets/process_data_output_0215/try_step_4/step_4_fim_critique_0217_try_0218.json \
+      --shard $i --total-shards 200 \
+      --wandb --wandb-run-name "exp_0218" \
+      > shard_$i.log 2>&1 &
+    sleep 60
+done
+
