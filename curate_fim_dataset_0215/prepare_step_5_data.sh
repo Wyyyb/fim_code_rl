c7fd6458632584bc36a75722e2a8eae13ccd3a51
feishan <feishan@minimaxi.com>
2026-02-20 10:14:18 +0800

    add

---
+++ curate_fim_dataset_0215/prepare_step_5_data.sh
@@ -0,0 +1,4 @@
+cp /data/yubo/datasets/process_data_output_0215/step_4_fim_critique_0217_checkpoint_shard*.json /data/yubo/datasets/temp_data_0219/
+
+cp /data/yubo/datasets/process_data_output_0215/try_step_4/step_4_fim_critique_0217_try_0218_checkpoint_shard*.json /data/yubo/datasets/temp_data_0219/
+
