8cae0efae03345de35fb36528ccc0d8b0fb071ef
feishan <feishan@minimaxi.com>
2026-02-23 15:03:28 +0800

    add

--- curate_fim_dataset_0215/multi_start_step_4.sh
+++ curate_fim_dataset_0215/multi_start_step_4.sh
@@ -1,6 +1,6 @@
 
 
-for i in $(seq 1 50); do
+for i in $(seq 1 30); do
     nohup python step_4_gemini_fim_and_critique_0217.py \
       -i /data/yubo/datasets/process_data_output_0215/step_3_selected_fim_functions_0215_functions.json \
       -o /data/yubo/datasets/process_data_output_0215/step_4_fim_critique_0217.json \
