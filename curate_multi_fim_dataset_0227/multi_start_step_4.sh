ca181116a2b39af5cdfef3c19f61bce6078e6a5c
feishan <feishan@minimaxi.com>
2026-02-27 23:12:55 +0800

    add

--- curate_multi_fim_dataset_0227/multi_start_step_4.sh
+++ curate_multi_fim_dataset_0227/multi_start_step_4.sh
@@ -4,11 +4,12 @@
 #  --total-shards 200
 
 # 再并行跑（每个worker只读自己那个小shard文件）
-for i in $(seq 1 5); do
+for i in $(seq 6 200); do
   nohup python step_4_multi_fim_gemini_0227.py \
     -i /data/yubo/datasets/process_data_output_0227/step_3_selected_multi_fim_functions_0227_groups.json \
     -o /data/yubo/datasets/process_data_output_0227/step_4_multi_fim_output_0227.json \
     --shard $i --total-shards 200 > shard_$i.log 2>&1 &
+  sleep 10
 done
 
 
