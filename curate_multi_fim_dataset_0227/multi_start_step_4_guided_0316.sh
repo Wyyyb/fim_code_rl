3c623da16833313f7d2041123c2f6fd08c0dd79b
feishan <feishan@minimaxi.com>
2026-03-16 12:41:14 +0800

    add

--- curate_multi_fim_dataset_0227/multi_start_step_4_guided_0316.sh
+++ curate_multi_fim_dataset_0227/multi_start_step_4_guided_0316.sh
@@ -1,6 +1,6 @@
 
 for i in $(seq 1 50); do
-    nohup step_4_multi_fim_gemini_0227_guided_0316.py \\
+    nohup python step_4_multi_fim_gemini_0227_guided_0316.py \\
       -i /data/yubo/datasets/process_data_output_0227/step_3_selected_multi_fim_functions_0227_groups.json \\
       -o /data/yubo/datasets/process_data_output_0316/step_4_multi_fim_output_guided_0316.json \\
       --shard $i --total-shards 200 --guided \\
