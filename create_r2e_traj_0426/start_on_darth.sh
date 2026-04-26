11509bb2fcd87c5f0483a35905cd80c8205dfd33
feishan <feishan@minimaxi.com>
2026-04-26 14:19:54 +0800

    add

--- create_r2e_traj_0426/start_on_darth.sh
+++ create_r2e_traj_0426/start_on_darth.sh
@@ -7,7 +7,7 @@ uv run python src/r2egym/agenthub/run/edit.py runagent_multiple \
   --k 4578 \
   --dataset "R2E-Gym/R2E-Gym-Lite" \
   --split "train" \
-  --llm_name 'gemini-3-flash-preview' \
+  --llm_name 'gemini/gemini-3-flash-preview' \
   --use_fn_calling True \
   --exp_name r2egym-training-trajectories \
   --temperature 0.2 \
